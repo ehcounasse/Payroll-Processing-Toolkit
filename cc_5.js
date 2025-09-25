e5b9e76e699a4457490ff938206c786355a42983
Ethan Counasse <ehcounasse@usf.edu>
2025-09-25 12:38:40 -0400

    Created Array

---
+++ cc_5.js
@@ -0,0 +1,6 @@
+let employee = [
+    {name: "John", hourlyRate: 12, hoursWorked: 40},
+    {name: "Drew", hourlyRate: 14, hourWorked: 38},
+    {name: "Lisa", hourlyRate: 11, hoursWorked: 36}
+];
+
