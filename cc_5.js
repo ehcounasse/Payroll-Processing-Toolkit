deba65b9d73d15811ae7a21be6008cf885663f4c
Ethan Counasse <ehcounasse@usf.edu>
2025-09-25 13:29:34 -0400

    Base Pay Calculation Function

--- cc_5.js
+++ cc_5.js
@@ -1,6 +1,21 @@
 let employee = [
     {name: "John", hourlyRate: 12, hoursWorked: 40},
-    {name: "Drew", hourlyRate: 14, hourWorked: 38},
-    {name: "Lisa", hourlyRate: 11, hoursWorked: 36}
+    {name: "Drew", hourlyRate: 14, hoursWorked: 45},
+    {name: "Lisa", hourlyRate: 11, hoursWorked: 41}
 ];
+
+function calculateBasePay(rate, hours) {
+    let regHours = 0;
+    if (hours > 40 ) {
+        regHours = 40
+     } else {
+        regHours = hours 
+     }
+     return rate * regHours
+};
+     
+
+employee.forEach(emp => {
+    const basePay = calculateBasePay (emp.hourlyRate, emp.hoursWorked)
+console.log (`${emp.name} makes $${basePay} as their base pay`)});
 
